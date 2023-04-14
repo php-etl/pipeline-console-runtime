cc3f3555e89a590624f119789260ad1dd9fff8ac
sebprt <sebastien@kiboko.fr>
2023-04-14 16:41:16 +0200

    Used the RunnableInterface provided by the package php-etl/job-contracts

--- src/PipelineRuntimeInterface.php
+++ src/PipelineRuntimeInterface.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Kiboko\Component\Runtime\Pipeline;
 
+use Kiboko\Contract\Job\RunnableInterface;
 use Kiboko\Contract\Pipeline\ExtractingInterface;
 use Kiboko\Contract\Pipeline\LoadingInterface;
-use Kiboko\Contract\Pipeline\RunnableInterface;
 use Kiboko\Contract\Pipeline\TransformingInterface;
 
 interface PipelineRuntimeInterface extends ExtractingInterface, TransformingInterface, LoadingInterface, RunnableInterface
